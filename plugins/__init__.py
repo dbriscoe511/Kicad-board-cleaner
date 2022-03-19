4a2f7ac990aa2b9cfba13631ccb6e6a353c9b142
Dbriscoe511 <extradrivespace123456@gmail.com>
2022-03-19 16:41:16 -0600

    corrected __init__

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,35 +1,11 @@
+# -*- coding: utf-8 -*-
+try:
+    # Note the relative import!
+    from .action_board_clean import clean_dialog
+    # Instantiate and register to Pcbnew
+    clean_dialog().register()
+    # if failed, log the error and let the user know
+except Exception as e:
 
-import board_clean
-import Clean_board_GUI
-
-class clean_dialog():
-    def __init__(self,parent):
-        #this class inherets dialog
-        super(Clean_board_GUI, self).__init__(parent)
-
-        mop = board_clean.board_cleaner()
-    def on_ok(self,event):
-
-        #get checkboxes
-        hide_des        = self.chkbox_hide_des.GetValue()
-        hide_des_fab    = self.chkbox_hide_des_fab.GetValue()
-        unhide_des      = self.chkbox_exclude_des.GetValue()
-        hide_val        = self.chkbox_hide_val.GetValue()
-        unhide_val      = self.chkbox_exclude_val.GetValue()
-
-        #get lists and split
-        des_list = self.Des_hide_list.GetValue()
-        des_list = des_list.split(',')
-
-        val_list = self.val_hide_list.GetValue()
-        val_list = val_list.split(',')
-
-        #clean
-        self.mop.hide(hide_des, hide_val, des_list, val_list, hide_des_fab, unhide_des, unhide_val )
-
-
-    def on_cancel(self, event):
-        event.Skip()
-        self.Destroy()
-
-
+    import wx
+    wx.MessageBox('cleaner', f"Something went wrong: \n {e}", wx.OK | wx.ICON_ERROR)
---
+++ plugins/action_board_clean.py
@@ -0,0 +1,35 @@
+
+from .board_clean import board_cleaner
+from .Clean_board_GUI import clean_boardGUI
+
+class clean_dialog():
+    def __init__(self,parent):
+        #this class inherets dialog
+        super(clean_boardGUI, self).__init__(parent)
+
+        mop = board_cleaner()
+    def on_ok(self,event):
+
+        #get checkboxes
+        hide_des        = self.chkbox_hide_des.GetValue()
+        hide_des_fab    = self.chkbox_hide_des_fab.GetValue()
+        unhide_des      = self.chkbox_exclude_des.GetValue()
+        hide_val        = self.chkbox_hide_val.GetValue()
+        unhide_val      = self.chkbox_exclude_val.GetValue()
+
+        #get lists and split
+        des_list = self.Des_hide_list.GetValue()
+        des_list = des_list.split(',')
+
+        val_list = self.val_hide_list.GetValue()
+        val_list = val_list.split(',')
+
+        #clean
+        self.mop.hide(hide_des, hide_val, des_list, val_list, hide_des_fab, unhide_des, unhide_val )
+
+
+    def on_cancel(self, event):
+        event.Skip()
+        self.Destroy()
+
+
